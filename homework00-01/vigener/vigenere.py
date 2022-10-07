e5f043541c6db5a6c7738d863dbad528044f45be
Ulya <morozova.uk03@gmail.com>
2022-10-07 11:37:44 +0300

    vigenere

--- homework00-01/vigener/vigenere.py
+++ homework00-01/vigener/vigenere.py
@@ -18,15 +18,27 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
         else:
             keyword += keyword[:a]
     keyword = keyword.upper()
+    k = ""
+    if plaintext == plaintext.lower():
+        plaintext = plaintext.upper()
+        k = 1
+    elif plaintext == plaintext.capitalize():
+        plaintext = plaintext.upper()
+        k = 0
     for i in range(len(plaintext)):
         shift = ord(keyword[i]) - 65
         p = ord(plaintext[i])
-        p += shift
-        if p > 122:
-            p -= 26
-        elif p > 90:
-            p -= 26
+        if p >=65 and p < 91:
+            p += shift
+            if p > 90:
+                p -= 26
+        elif p >= 91:
+            p += shift
         ciphertext += chr(p)
+    if k:
+        ciphertext = ciphertext.lower()
+    elif k == 0:
+        ciphertext = ciphertext.capitalize()
     return ciphertext
 
 
@@ -50,17 +62,29 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
         else:
             keyword += keyword[:a]
     keyword = keyword.upper()
+    k = ""
+    if ciphertext == ciphertext.lower():
+        ciphertext = ciphertext.upper()
+        k = 1
+    elif ciphertext == ciphertext.capitalize():
+        ciphertext = ciphertext.upper()
+        k = 0
     for i in range(len(ciphertext)):
         shift = ord(keyword[i]) - 65
         p = ord(ciphertext[i])
-        p -= shift
-        if p < 65:
-            p += 26
-        if p > 90 and p < 97:
-            p += 26
+        if p >= 65 and p < 91:
+            p -= shift
+            if p < 65:
+                p += 26
+        elif p > 90:
+            p -= shift
         plaintext += chr(p)
+    if k:
+        plaintext = plaintext.lower()
+    elif k == 0:
+        plaintext = plaintext.capitalize()
     return plaintext
 
-ex1 = encrypt_vigenere("ATTACKATDAWN", "LEMON")
+ex1 = encrypt_vigenere("python", "a")
 ex2 = decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
 print(ex1, ex2)
